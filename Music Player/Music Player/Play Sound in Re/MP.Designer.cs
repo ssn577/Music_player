3351add1e053c482bcc52ad55446acf4d2e1ace9
AliPlvP <47819064+ssn577@users.noreply.github.com>
2022-07-09 13:22:39 +0430

    Add files via upload

---
+++ Music Player/Music Player/Play Sound in Re/MP.Designer.cs
@@ -0,0 +1,225 @@
+﻿
+namespace Play_Sound_in_Re
+{
+    partial class frm_mp
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frm_mp));
+            this.wmp = new AxWMPLib.AxWindowsMediaPlayer();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.btn_add = new System.Windows.Forms.Button();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.lst_music = new System.Windows.Forms.ListBox();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.icon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.icon_menu = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.openProgramToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)(this.wmp)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox4.SuspendLayout();
+            this.icon_menu.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // wmp
+            // 
+            this.wmp.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.wmp.Enabled = true;
+            this.wmp.Location = new System.Drawing.Point(6, 19);
+            this.wmp.Name = "wmp";
+            this.wmp.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("wmp.OcxState")));
+            this.wmp.Size = new System.Drawing.Size(352, 187);
+            this.wmp.TabIndex = 0;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.Filter = "mp3 files (*.mp3)|*.mp3";
+            this.openFileDialog1.Title = "Add to list";
+            // 
+            // btn_add
+            // 
+            this.btn_add.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btn_add.Location = new System.Drawing.Point(6, 69);
+            this.btn_add.Name = "btn_add";
+            this.btn_add.Size = new System.Drawing.Size(111, 34);
+            this.btn_add.TabIndex = 2;
+            this.btn_add.Text = "Add to list";
+            this.btn_add.UseVisualStyleBackColor = true;
+            this.btn_add.Click += new System.EventHandler(this.btn_add_Click);
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.linkLabel1.Location = new System.Drawing.Point(6, 16);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(111, 17);
+            this.linkLabel1.TabIndex = 5;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "https://www.aliplvp.ir";
+            this.linkLabel1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox1.Controls.Add(this.groupBox3);
+            this.groupBox1.Controls.Add(this.groupBox2);
+            this.groupBox1.Location = new System.Drawing.Point(12, 230);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(364, 125);
+            this.groupBox1.TabIndex = 6;
+            this.groupBox1.TabStop = false;
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox3.Controls.Add(this.linkLabel1);
+            this.groupBox3.Controls.Add(this.btn_add);
+            this.groupBox3.Location = new System.Drawing.Point(235, 7);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(123, 112);
+            this.groupBox3.TabIndex = 7;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Control";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox2.Controls.Add(this.lst_music);
+            this.groupBox2.Location = new System.Drawing.Point(6, 7);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(223, 112);
+            this.groupBox2.TabIndex = 7;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Playlist";
+            // 
+            // lst_music
+            // 
+            this.lst_music.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lst_music.FormattingEnabled = true;
+            this.lst_music.Location = new System.Drawing.Point(6, 21);
+            this.lst_music.Name = "lst_music";
+            this.lst_music.Size = new System.Drawing.Size(211, 82);
+            this.lst_music.TabIndex = 1;
+            this.lst_music.DoubleClick += new System.EventHandler(this.lst_music_DoubleClick);
+            // 
+            // groupBox4
+            // 
+            this.groupBox4.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox4.Controls.Add(this.wmp);
+            this.groupBox4.Location = new System.Drawing.Point(12, 12);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Size = new System.Drawing.Size(364, 212);
+            this.groupBox4.TabIndex = 7;
+            this.groupBox4.TabStop = false;
+            this.groupBox4.Text = "Player";
+            // 
+            // icon1
+            // 
+            this.icon1.ContextMenuStrip = this.icon_menu;
+            this.icon1.Icon = ((System.Drawing.Icon)(resources.GetObject("icon1.Icon")));
+            this.icon1.Text = "Music Player";
+            this.icon1.DoubleClick += new System.EventHandler(this.icon1_DoubleClick);
+            // 
+            // icon_menu
+            // 
+            this.icon_menu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openProgramToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.icon_menu.Name = "icon_menu";
+            this.icon_menu.ShowImageMargin = false;
+            this.icon_menu.Size = new System.Drawing.Size(128, 48);
+            // 
+            // openProgramToolStripMenuItem
+            // 
+            this.openProgramToolStripMenuItem.Name = "openProgramToolStripMenuItem";
+            this.openProgramToolStripMenuItem.Size = new System.Drawing.Size(127, 22);
+            this.openProgramToolStripMenuItem.Text = "Open Program";
+            this.openProgramToolStripMenuItem.Click += new System.EventHandler(this.icon1_DoubleClick);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(127, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // frm_mp
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(388, 365);
+            this.Controls.Add(this.groupBox4);
+            this.Controls.Add(this.groupBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "frm_mp";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Music Player";
+            this.Resize += new System.EventHandler(this.frm_mp_Resize);
+            ((System.ComponentModel.ISupportInitialize)(this.wmp)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox4.ResumeLayout(false);
+            this.icon_menu.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button btn_add;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.ListBox lst_music;
+        private System.Windows.Forms.GroupBox groupBox4;
+        private AxWMPLib.AxWindowsMediaPlayer wmp;
+        private System.Windows.Forms.NotifyIcon icon1;
+        private System.Windows.Forms.ContextMenuStrip icon_menu;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openProgramToolStripMenuItem;
+    }
+}
+
---
+++ Music Player/Music Player/Play Sound in Re/MP.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Windows.Forms;
+
+namespace Play_Sound_in_Re
+{
+    public partial class frm_mp : Form
+    {
+        public frm_mp()
+        {
+            InitializeComponent();
+        }
+
+        string file;
+        List<string> path = new List<string>();
+        private void btn_add_Click(object sender, EventArgs e)
+        {            
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                file = openFileDialog1.SafeFileName;
+                path.Add(openFileDialog1.FileName);
+                lst_music.Items.Add(file);
+            }
+        }
+
+        private void lst_music_DoubleClick(object sender, EventArgs e)
+        {
+            try
+            {
+                wmp.URL = path[lst_music.SelectedIndex];
+            }
+            catch
+            {
+                MessageBox.Show("No file was selected","Music Player",MessageBoxButtons.OK,MessageBoxIcon.Error);
+            }
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            ProcessStartInfo sInfo = new ProcessStartInfo("http://www.aliplvp.ir");
+            Process.Start(sInfo);
+        }
+
+        private void frm_mp_Resize(object sender, EventArgs e)
+        {
+            if (WindowState == FormWindowState.Minimized)
+            {
+                this.Hide();
+                icon1.Visible = true;
+            }
+            else
+            {
+                icon1.Visible = false;
+            }
+        }
+
+        private void icon1_DoubleClick(object sender, EventArgs e)
+        {
+            this.Show();
+            this.WindowState = FormWindowState.Normal;
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
